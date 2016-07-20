dac1c629c5117e890587a067a8b8e9699b97e4e4
jordan4ibanez <jordan4ibanez@gmail.com>
2016-07-20 14:19:35 -0400

    Update textures and code

--- init.lua
+++ init.lua
@@ -5,18 +5,26 @@ scuba = {
 }
 
 scuba.set = function(defs)
+	--set the scuba hud
 	if scuba.huds[defs.player][1] == nil then
-      local hud_id=defs.player:hud_add({
-        hud_elem_type = "image",
-        position = {x = 0.5, y = 0.5},
-        scale = {
-          x = -101,
-          y = -101
-        },
-        text = "scuba.png"
-      })
-      scuba.huds[defs.player][1] = hud_id;
-     end
+		local hud_id=defs.player:hud_add({
+		hud_elem_type = "image",
+		position = {x = 0.5, y = 0.5},
+		scale = {
+			x = -101,
+			y = -101
+			},
+			text = "scuba.png"
+		})
+		scuba.huds[defs.player][1] = hud_id;
+	--hard override 3d armor not knowing external pieces of armor (causes debug spam)
+	elseif not string.match(armor["textures"][defs.player:get_player_name()].armor, "^scuba_helmet.png") then
+		local player = defs.player
+		local armor_texture = armor["textures"][player:get_player_name()].armor
+		armor_texture = armor_texture.."^scuba_helmet.png"
+		armor["textures"][player:get_player_name()].armor = armor_texture
+		armor.update_player_visuals(armor,player)
+	end
 end
 
 scuba.add = function (defs)
@@ -35,41 +43,22 @@ scuba.remove = function (defs)
   scuba.del(defs)
 end
 
---if there's no armor slots, player has to hold the item to use it
-if minetest.get_modpath("3d_armor") == nil then
-	minetest.register_globalstep(function(dtime)
-		for _,player in ipairs(minetest.get_connected_players()) do
-			if player:get_wielded_item():to_table() ~= nil then
-				if player:get_wielded_item():to_table().name == "scuba:scuba_helmet" then
-					scuba.set({player = player})
-					player:set_breath(10)
-				else
-					scuba.del({player = player})
-				end
-			else
-				scuba.del({player = player})
+--make this do something more efficient
+minetest.register_globalstep(function(dtime)
+	for _,player in ipairs(minetest.get_connected_players()) do
+		local inv = player:get_inventory()
+		for i=1, 6 do
+			local stack = inv:get_stack("armor", i)
+			local item = stack:get_name()
+			if item == "scuba:scuba_helmet" then
+				scuba.set({player = player})
+				player:set_breath(10)
+				return
 			end
 		end
-	end)
-else
-
-	minetest.register_globalstep(function(dtime)
-		for _,player in ipairs(minetest.get_connected_players()) do
-			local inv = player:get_inventory()
-			for i=1, 6 do
-				local stack = inv:get_stack("armor", i)
-				local item = stack:get_name()
-				if item == "scuba:scuba_helmet" then
-					player:set_breath(10)
-					scuba.set({player = player})
-					return
-				end
-			end
-			scuba.del({player = player})
-		end
-	end)
-end
-
+		scuba.del({player = player})
+	end
+end)
 
 minetest.register_on_joinplayer(function (player)
   scuba.huds[player] = {}
@@ -77,5 +66,5 @@ end)
 
 minetest.register_craftitem("scuba:scuba_helmet", {
 	description = "Scuba Helmet",
-	inventory_image = "scuba.png",
+	inventory_image = "scuba_item.png",
 })
